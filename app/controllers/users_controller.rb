4b978dcb3ac6d199cd4859a13a42549cfac93526
msenior526 <msenior526@gmail.com>
2021-04-04 14:34:15 -0400

    Create session when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,15 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            session[user_id] = user.id
             redirect_to user
         else
-            byebug
             render :new
         end
     end
 
     def show
-       
+
     end
 
     def edit
