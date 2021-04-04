da65a79050cfc46067ce446362e3471a2f34cbbe
msenior526 <msenior526@gmail.com>
2021-04-04 13:31:57 -0400

    Add validations to user model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,7 @@ class Review < ApplicationRecord
   belongs_to :strain
 
   validates :context, :rating, presence: true
+  validates :context, length: {maximum: 200}
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 11 }
   validates_associated :strain, :user
 end
