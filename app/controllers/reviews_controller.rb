4ac4cd1ae7cce36bc17e11fe4f568769fb5cd610
msenior526 <msenior526@gmail.com>
2021-04-04 17:14:01 -0400

    Add private method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,15 @@
 class ReviewsController < ApplicationController
     def new
+        redirect_if_not_logged_in
+        @review = Review.new
     end
 
     def create
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:context, :rating, :user_id, :strain_id)
     end
 end
