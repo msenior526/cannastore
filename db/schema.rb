8a220c6725cbe2ab01c437111a6d47210e380469
msenior526 <msenior526@gmail.com>
2021-04-04 12:27:45 -0400

    Use rails generator to create review model

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_04_04_161851) do
 
   create_table "categories", force: :cascade do |t|
-    t.string "type"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
