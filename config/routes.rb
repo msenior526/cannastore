edb9cd8d4a2087010b91dfa9ce87cf2f1b1f38f0
msenior526 <msenior526@gmail.com>
2021-04-04 13:49:46 -0400

    Create routes, and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  resources :strains do 
+    resources :reviews, only: [:index]
+  end
+
+  resources :users, except: [:new, :create, :index]
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#delete'
 end
