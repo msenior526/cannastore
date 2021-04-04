3ae4dcff84f440f25955bc82a58f7056fbab1618
msenior526 <msenior526@gmail.com>
2021-04-04 12:20:03 -0400

    Create category model with rails generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_160416) do
+ActiveRecord::Schema.define(version: 2021_04_04_161851) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "strains", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "thc_content"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_strains_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_04_04_160416) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "strains", "categories"
 end
