e7a8b2a37944fe342594725302cd670f3aa2cb38
msenior526 <msenior526@gmail.com>
2021-04-04 15:40:58 -0400

    Create method to redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
       def logged_in?
         !!session[:user_id]
       end
+
+      def redirect_if_not_logged_in
+        if !logged_in?
+          redirect_to login_path
+        end
+      end
 end
