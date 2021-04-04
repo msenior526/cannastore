a2f73512095c796d2625af1fd32503159215afa4
msenior526 <msenior526@gmail.com>
2021-04-04 13:17:58 -0400

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :strains, through: :reviews
     has_many :favorite_strains
     # has_many :favorites, through: :favorite_strains, source: strain
-
     has_secure_password
+
+    validates :username, :email, presence: true, uniqueness: true
+    validates :password, :birthday, presence: true
 end
