de079a8e364ed34f0d8e2e482555aee079d0f8dc
msenior526 <msenior526@gmail.com>
2021-04-04 21:22:12 -0400

    Add route for third party authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+
   resources :strains do 
     resources :reviews
   end
