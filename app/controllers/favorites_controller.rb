edc011235bc5375ac4e5b76e60431ec49cae6584
msenior526 <msenior526@gmail.com>
2021-04-09 17:09:37 -0400

    Edit index action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,4 +1,10 @@
 class FavoritesController < ApplicationController
     def index
+        if logged_in?
+            @user = User.find_by(id: session[:user_id])
+            @favorites = @user.favorites
+        else
+            redirect_if_not_logged_in
+        end
     end
 end
