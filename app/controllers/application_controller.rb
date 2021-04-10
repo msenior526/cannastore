b58f5ef8b4583669cf35b8dc760a85d5ad49dff7
msenior526 <msenior526@gmail.com>
2021-04-10 10:21:39 -0400

    Refactor into helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-      end
-    
-      def logged_in?
-        !!session[:user_id]
-      end
-
-      def redirect_if_not_logged_in
-        if !logged_in?
-          redirect_to login_path
-        end
-      end
-
-      def redirect_if_logged_in
-        if logged_in?
-          redirect_to user_path(current_user)
-        end
-      end
+  include ApplicationHelper
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+      end
+    
+      def logged_in?
+        !!current_user
+      end
+
+      def redirect_if_not_logged_in
+        if !logged_in?
+          redirect_to login_path
+        end
+      end
+
+      def redirect_if_logged_in
+        if logged_in?
+          redirect_to user_path(current_user)
+        end
+      end
 end
