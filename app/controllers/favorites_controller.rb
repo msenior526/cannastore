2d88e1ca77db04412c682fabf5c97bc6ada13c1f
msenior526 <msenior526@gmail.com>
2021-04-10 08:39:25 -0400

    Refactor

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -11,12 +11,23 @@ class FavoritesController < ApplicationController
     def create
         @user = current_user
         @strain = Strain.find_by(id: params[:strain_id])
-        if !@user.favorites.include?(@strain)
-            favorite = FavoriteStrain.create(user_id: params[:user_id], strain_id: @strain.id)
+        if add_favorite(@user, @strain)
             redirect_to user_favorites_path(@user)
         else 
-             FavoriteStrain.where(["user_id = ? and strain_id = ?", @user.id, @strain.id]).delete_all
-             redirect_to user_favorites_path(@user)
+            remove_favorite(@user, @strain)
+            redirect_to user_favorites_path(@user)
+        end
+    end
+
+    private
+
+    def remove_favorite(user, strain)
+        FavoriteStrain.where(["user_id = ? and strain_id = ?", user.id, strain.id]).delete_all
+    end
+
+    def add_favorite(user, strain)
+        if !user.favorites.include?(strain)
+            FavoriteStrain.create(user_id: user.id, strain_id: strain.id)
         end
     end
 end
