02ea1f57c53dd29707603f9f763a922d8fdb037c
msenior526 <msenior526@gmail.com>
2021-04-10 14:18:00 -0400

    Insert form for age verification

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
     def home
+        if params[:choice] == "yes"
+            session[:choice_id]
+            redirect_to strains_path
+        elsif params[:choice] == "no"
+            redirect_to :home, alert: "You must be 21 to enter!"
+        end
     end
 end
