e0bd235ad1c6784b483ef119402828838e75fc12
msenior526 <msenior526@gmail.com>
2021-04-13 20:12:43 -0400

    Move action from welcome to static

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
     def home
+        def home
+            if params[:choice] == "yes"
+                session[:choice_id]
+                redirect_to strains_path
+            elsif params[:choice] == "no"
+                redirect_to :root, alert: "You must be 21 to enter!"
+            end
+        end
     end
 
     def contact
