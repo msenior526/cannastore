01344fa927e9219e0dc8b24353cc6bf57458f88a
msenior526 <msenior526@gmail.com>
2021-04-13 20:13:38 -0400

    Edit action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,10 @@
 class StaticPagesController < ApplicationController
     def home
-        def home
-            if params[:choice] == "yes"
-                session[:choice_id]
-                redirect_to strains_path
-            elsif params[:choice] == "no"
-                redirect_to :root, alert: "You must be 21 to enter!"
-            end
+        if params[:choice] == "yes"
+            session[:choice_id]
+            redirect_to strains_path
+        elsif params[:choice] == "no"
+            redirect_to :root, alert: "You must be 21 to enter!"
         end
     end
 
