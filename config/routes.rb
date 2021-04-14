c45c447720f124a4aabb74b8fb16728c4f4c085b
msenior526 <msenior526@gmail.com>
2021-04-13 20:11:04 -0400

    Add routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root 'welcome#home'
+  root 'static#home'
+  get '/about', to: 'static#about'
+  get '/contact', to: 'static#contact'
+  
   get '/auth/:provider/callback', to: 'sessions#omniauth'
 
   resources :strains do 
