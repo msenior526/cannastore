983e55a30f7861321f23c56e4e9760d8e80b1b55
msenior526 <msenior526@gmail.com>
2021-04-15 21:30:41 -0400

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,22 @@ Rails.application.routes.draw do
   
   get '/auth/:provider/callback', to: 'sessions#omniauth'
 
-  resources :strains do 
-    resources :reviews
-  end
-  get '/most_reviewed', to: 'strains#most_reviewed'
-
-  resources :categories, only: [:index, :show]
-
-  resources :users, except: [:new, :create, :index] do
-    resources :favorites, only: [:index, :create]
-  end
-
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
+
+  get '/most_reviewed', to: 'strains#most_reviewed'
+  
+  resources :strains do 
+    resources :reviews
+  end
+  
+  resources :categories, only: [:index, :show]
+
+  resources :users, except: [:new, :create, :index] do
+    resources :favorites, only: [:index, :create]
+  end
 end
