ee593b0003c02d389b7df1dc469bd7102e875e8d
msenior526 <msenior526@gmail.com>
2021-04-20 09:11:36 -0400

    Create a 404 error method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :warning, :info
   include ApplicationHelper
   include StrainsHelper
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
 end
