1a283ab883fbf39f6afeeb0682199e6d51e4bbf9
msenior526 <msenior526@gmail.com>
2021-04-22 13:30:45 -0400

    Refactor using helper

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -5,4 +5,12 @@ module FavoritesHelper
             render partial: 'favorites/form'
         end 
     end
+
+    def display_favorites(favorites)
+        if favorites.empty?
+            content_tag(:p, "You have not favorited any strains yet.")
+        else
+            render partial: 'strains/strain', collection: favorites
+        end
+    end
 end
