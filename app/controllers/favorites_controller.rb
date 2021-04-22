53ba762dc65cd771dabc993a9190cd14a124e61c
msenior526 <msenior526@gmail.com>
2021-04-21 22:46:55 -0400

    Refactor

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,16 @@
 class FavoritesController < ApplicationController
     def index
         if logged_in?
-            @user = User.find_by(id: session[:user_id])
-            @favorites = @user.favorites
+            @favorites = current_user.favorites
         else
             redirect_if_not_logged_in
         end
     end
 
     def create
-        @user = current_user
         @strain = Strain.find_by(id: params[:strain_id])
-        add_or_remove_favorite(@user, @strain)
-        redirect_to user_favorites_path(@user)
+        add_or_remove_favorite(current_user, @strain)
+        redirect_to user_favorites_path(current_user)
     end
 
     private
