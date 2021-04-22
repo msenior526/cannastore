0e898d51b072cf733df1508e4545623908a93e9c
msenior526 <msenior526@gmail.com>
2021-04-22 13:24:40 -0400

    Remove method from helper into controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -18,4 +18,12 @@ class FavoritesController < ApplicationController
     def favorite_params
         params.permit(:user_id, :strain_id)
     end
+
+    def add_or_remove_favorite(user, strain)
+        if !user.favorites.include?(strain)
+            FavoriteStrain.create(favorite_params)
+        else
+            FavoriteStrain.where(["user_id = ? and strain_id = ?", user.id, strain.id]).delete_all
+        end
+    end
 end
--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,13 +1,5 @@
 module FavoritesHelper
 
-    def add_or_remove_favorite(user, strain)
-        if !user.favorites.include?(strain)
-            FavoriteStrain.create(favorite_params)
-        else
-            FavoriteStrain.where(["user_id = ? and strain_id = ?", user.id, strain.id]).delete_all
-        end
-    end
-
     def favorite_button
         if logged_in?
             render partial: 'favorites/form'
